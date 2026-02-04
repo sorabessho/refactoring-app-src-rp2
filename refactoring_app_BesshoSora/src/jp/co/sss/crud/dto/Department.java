df9e1a7da3c82442ad200d2ebe23d8f821642d32
sorabessho <sora_bessho@asupage.co.jp>
2026-02-04 17:12:40 +0900

    fix:Employee.java,Department.java DTOクラスに記述の追加
    狙い:保守性の向上
    両DTOクラスに、フィールド、getter、setter、ToStringのオーバーライドを実装。

--- refactoring_app_BesshoSora/src/jp/co/sss/crud/dto/Department.java
+++ refactoring_app_BesshoSora/src/jp/co/sss/crud/dto/Department.java
@@ -1,5 +1,27 @@
 package jp.co.sss.crud.dto;
 
 public class Department {
+	private int deptId;
+	private String deptName;
 
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [deptId=" + deptId + ", deptName=" + deptName + "]";
+	}
 }
--- refactoring_app_BesshoSora/src/jp/co/sss/crud/dto/Employee.java
+++ refactoring_app_BesshoSora/src/jp/co/sss/crud/dto/Employee.java
@@ -1,5 +1,81 @@
 package jp.co.sss.crud.dto;
 
 public class Employee {
+	private int empId;
+	private String empName;
+	private int gender;
+	private String birthday;
+	private Department department;
 
+	public int getEmpId() {
+		return empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public String getGender() {
+		//性別の文字化
+		String genderString;
+		if (this.gender == 0) {
+			genderString = "回答なし";
+		} else if (this.gender == 1) {
+			genderString = "男性";
+		} else if (this.gender == 2) {
+			genderString = "女性";
+		} else if (this.gender == 9) {
+			genderString = "その他";
+		} else {
+			genderString = "不明";
+		}
+		return genderString;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		//性別の文字化
+		String genderString;
+		if (this.gender == 0) {
+			genderString = "回答なし";
+		} else if (this.gender == 1) {
+			genderString = "男性";
+		} else if (this.gender == 2) {
+			genderString = "女性";
+		} else if (this.gender == 9) {
+			genderString = "その他";
+		} else {
+			genderString = "不明";
+		}
+
+		return empId + "\t" + empName + "\t" + genderString + "\t" + birthday + "\t" + department.getDeptName();
+	}
 }
