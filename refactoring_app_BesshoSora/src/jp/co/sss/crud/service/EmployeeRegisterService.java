9a2de48af79988e9a6e169bbae4f6a7b9983b045
sorabessho <sora_bessho@asupage.co.jp>
2026-02-06 13:40:19 +0900

    fix:EmployeeRegisterService.java 社員情報登録のサービスメソッド追加
    狙い:保守性の向上

--- refactoring_app_BesshoSora/src/jp/co/sss/crud/service/EmployeeRegisterService.java
+++ refactoring_app_BesshoSora/src/jp/co/sss/crud/service/EmployeeRegisterService.java
@@ -1,5 +1,56 @@
 package jp.co.sss.crud.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.util.ConstantMsg;
+
 public class EmployeeRegisterService {
 
+	/**インスタンス化の禁止*/
+	private EmployeeRegisterService() {
+	}
+
+	/**
+	 * 社員情報を1件登録
+	 * 
+	 * @author 別所大空
+	 * @throws IOException 入力処理でエラーが発生した場合に送出
+	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
+	 * @throws SQLException DB処理でエラーが発生した場合に送出
+	 * @throws ParseException 形式処理でエラーが発生した場合に送出
+	 */
+	public static void registEmp() throws IOException, ClassNotFoundException, SQLException, ParseException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		//登録する値を入力
+		Employee employee = new Employee();
+		System.out.print(ConstantMsg.GUIDANCE_EMP_NAME);
+		employee.setEmpName(br.readLine());
+		System.out.print(ConstantMsg.GUIDANCE_GENDER);
+		employee.setGender(Integer.parseInt(br.readLine()));
+		System.out.print(ConstantMsg.GUIDANCE_BIRTHDAY);
+		employee.setBirthday(br.readLine());
+		System.out.print(ConstantMsg.GUIDANCE_DEPT_ID);
+		employee.getDepartment().setDeptId(Integer.parseInt(br.readLine()));
+
+		//機能の呼出
+		int result = EmployeeDAO.registEmpDAO(employee);
+
+		//処理完遂チェック（失敗の場合-result == 0）
+		if (result == 0) {
+			//登録失敗メッセージ
+			System.out.println(ConstantMsg.NOTICE_FAILED);
+		} else {
+			// 登録完了メッセージ
+			System.out.println(ConstantMsg.NOTICE_INSERT_COMPLETE);
+
+		}
+
+	}
 }
