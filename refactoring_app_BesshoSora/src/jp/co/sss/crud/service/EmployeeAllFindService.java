8335dbd0684b5c814bc40c2bb9d3446d66d72d44
sorabessho <sora_bessho@asupage.co.jp>
2026-02-06 11:18:03 +0900

    fix:EmployeeAllFindService.java findAll()の追加
    狙い:保守性の向上
    DAOから機能を呼出、コンソール出力を行う全件検索メソッドの追加。

--- refactoring_app_BesshoSora/src/jp/co/sss/crud/service/EmployeeAllFindService.java
+++ refactoring_app_BesshoSora/src/jp/co/sss/crud/service/EmployeeAllFindService.java
@@ -1,5 +1,36 @@
 package jp.co.sss.crud.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.util.ConstantMsg;
+
 public class EmployeeAllFindService {
 
+	/**
+	 * 全ての社員情報を検索
+	 * 
+	 * @author 別所大空
+	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
+	 * @throws SQLException DB処理でエラーが発生した場合に送出
+	 */
+	public static void findAll() throws ClassNotFoundException, SQLException {
+
+		//機能の呼出
+		List<Employee> employees = EmployeeDAO.findAllDAO();
+
+		//nullチェック
+		if (employees == null) {
+			System.out.println(ConstantMsg.NOTICE_FIND_COMPLETE_ANYONE);
+			return;
+		}
+
+		//従業員のコンソール出力
+		System.out.println(ConstantMsg.HEADER_FIND_RESULT);
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+	}
 }
