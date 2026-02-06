9a97f9858e2631eec8c4705c6d340cc62badc410
sorabessho <sora_bessho@asupage.co.jp>
2026-02-06 10:15:39 +0900

    fix:EmployeeDAO.java findByDeptIdDAO(int deptId)の追加。
    狙い:保守性の向上
    部署IDを条件に社員検索をし検索を返す、部署ID検索メソッドを追加。
    DB操作関連のみ。
    コンソール入出力は行わない。

--- refactoring_app_BesshoSora/src/jp/co/sss/crud/db/EmployeeDAO.java
+++ refactoring_app_BesshoSora/src/jp/co/sss/crud/db/EmployeeDAO.java
@@ -25,7 +25,7 @@ public class EmployeeDAO {
 	 * <DB操作>全ての社員情報を検索し結果を返す
 	 * 
 	 * @author 別所大空
-	 * @return List<Employee>（検索結果がある場合） null（検索結果がない場合）
+	 * @return List<Employee>（検索結果がある場合） OR null（検索結果がない場合）
 	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
 	 * @throws SQLException            DB処理でエラーが発生した場合に送出
 	 */
@@ -47,7 +47,7 @@ public class EmployeeDAO {
 				return null;
 			}
 
-			//検索結果をDTOListに入れる 修正-別所
+			//検索結果をDTOListに入れる
 			List<Employee> employees = new ArrayList<Employee>();
 			while (resultSet.next()) {
 				Employee employee = new Employee();
@@ -75,7 +75,7 @@ public class EmployeeDAO {
 	 * 
 	 * @author 別所大空
 	 * @param empName 社員名
-	 * @return List<Employee>（検索結果がある場合） null（検索結果がない場合）
+	 * @return List<Employee>（検索結果がある場合） OR null（検索結果がない場合）
 	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
 	 * @throws SQLException            DB処理でエラーが発生した場合に送出
 	 */
@@ -102,7 +102,62 @@ public class EmployeeDAO {
 				return null;
 			}
 
-			//検索結果をDTOListに入れる 修正-別所
+			//検索結果をDTOListに入れる
+			List<Employee> employees = new ArrayList<Employee>();
+			while (resultSet.next()) {
+				Employee employee = new Employee();
+				employee.setEmpId(resultSet.getInt("emp_id"));
+				employee.setEmpName(resultSet.getString("emp_name"));
+				employee.setGender(resultSet.getInt("gender"));
+				employee.setBirthday(resultSet.getString("birthday"));
+				employee.getDepartment().setDeptName(resultSet.getString("dept_name"));
+				employees.add(employee);
+			}
+			return employees;
+
+		} finally {
+			// クローズ処理
+			DBManager.close(resultSet);
+			// Statementをクローズ
+			DBManager.close(preparedStatement);
+			// DBとの接続を切断
+			DBManager.close(connection);
+		}
+	}
+
+	/**
+	 * <DB操作>部署IDに該当する社員情報を検索
+	 * 
+	 * @author 別所大空
+	 * @param deptId 部署ID
+	 * @return List<Employee>（検索結果がある場合） OR null（検索結果がない場合）
+	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
+	 * @throws SQLException            DB処理でエラーが発生した場合に送出
+	 */
+	public static List<Employee> findByDeptIdDAO(int deptId) throws ClassNotFoundException, SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// DBに接続
+			connection = DBManager.getConnection();
+			// SQL文を準備
+			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
+			sql.append(ConstantSQL.SQL_SELECT_BY_DEPT_ID);
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(sql.toString());
+			// 検索条件となる値をバインド
+			preparedStatement.setInt(1, deptId);
+			// SQL文を実行
+			resultSet = preparedStatement.executeQuery();
+
+			//結果が無しの場合
+			if (!resultSet.isBeforeFirst()) {
+				return null;
+			}
+
+			//検索結果をDTOListに入れる
 			List<Employee> employees = new ArrayList<Employee>();
 			while (resultSet.next()) {
 				Employee employee = new Employee();
